4191efa909811e401cb14aaf542e95108ee46ffe
Alozie hukwuemeka <chukwuemeka@curacel.ai>
2025-10-21 22:03:17 +0100

    Remove tasks from project before deleting

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -84,6 +84,7 @@ class ProjectController extends Controller
 
     public function destroy(Project $project)
     {
+        $project->tasks()->delete();
         $project->delete();
 
         return redirect()->route('projects.index')
